d1edbececa7bbddd685ee70dbaac44f7c9dfb6ed
dallasnorton <dallaslandonnorton@gmail.com>
2017-05-28 16:23:56 -0600

    location added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Location from './Location';
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Track My Trucker</h2>
         </div>
-        <p className="App-intro">
-          Lets check your location:
-        </p>
+        <Location />
       </div>
     );
   }
---
+++ src/Location.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Location extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: {}
+    };
+  }
+
+  componentDidMount() {
+    /* geolocation is available */
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.updateLocation(position)
+      });
+    }
+
+    this.watchId = navigator.geolocation.watchPosition((position) => {
+      this.updateLocation(position);
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  updateLocation(pos) {
+    console.log(pos);
+    this.setState({
+      currentLocation: pos.coords
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {"geolocation" in navigator ? (
+        <p>
+          Your location is: <span>{this.state.currentLocation.latitude}</span>, <span>{this.state.currentLocation.longitude}</span>
+        </p>
+        ) : (
+          <p>Geolocation is not supported by your browser</p>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Location;
